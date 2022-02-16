ee7cad017ef5fbb3292c69f28fdba09942ccf22a
ChristianMartinezTech <crismartine.e@gmail.com>
2022-02-15 22:12:50 -0500

    Started with 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that queries the Reddit API"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    '''returns the number of subscribers
+    for a given subreddit.'''
