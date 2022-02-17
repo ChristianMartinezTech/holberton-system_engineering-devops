a1f72dab569aaf9c054106a6b2f23a2704bb22ed
ChristianMartinezTech <crismartine.e@gmail.com>
2022-02-16 23:56:12 -0500

    Coded 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
-# Puttet script that fixes wrong extensions on assets loading
+# Puppet script that fixes wrong extensions on assets loading
 
 exec { 'Update loaded files extensions':
-    command => "sudo sed -i 's/.phpp/.php/g' wp-settings.php ",
-    path    => '/usr/bin',
-    cwd     => 'var/www/html'
+  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  provider => 'shell',
 }
