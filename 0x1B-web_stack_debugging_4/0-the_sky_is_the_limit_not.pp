d7a5a4fcc16f3cff449827c3f3c9b29338d084d6
ChristianMartinezTech <crismartine.e@gmail.com>
2022-02-24 10:16:33 -0500

    Uploading for checker feedback

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,7 +4,6 @@ exec { 'changelimit':
   path    => '/bin/',
   command => 'sed -i -e "s/15/1000/g" /etc/default/nginx'
 }
-
 service { 'nginx':
   ensure    => running,
   subscribe => Exec['changelimit']
